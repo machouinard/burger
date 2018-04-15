544dde291b6cb3b6b7e6d9f09f41a355c0113ea4
Mark Chouinard <mark@chouinard.me>
2018-04-15 15:20:25 -0700

    Reduce ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
 
-	const newIngredients = Object.keys( props.ingredients )
+	let newIngredients = Object.keys( props.ingredients )
 	                             .map( igKey => {
 	                             	return [...Array( props.ingredients[igKey] ) ].map( ( _, i ) => {
 	                             		return <BurgerIngredient key={igKey + i } type={igKey}/>
 	                                } );
-	                             } );
+	                             } ).reduce( (arr, el) => {
+	                             	return arr.concat( el );
+		}, []);
 
+	if ( 0 === newIngredients.length ) {
+		newIngredients = <p>Start building your Burger!</p>
+	}
 	console.log( 'ingredients', newIngredients );
 
 	return (
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,10 +6,10 @@ class BurgerBuilder extends Component {
 
 	state = {
 		ingredients: {
-			salad: 1,
-			bacon: 2,
-			cheese: 1,
-			meat: 2
+			salad: 0,
+			bacon: 0,
+			cheese: 0,
+			meat: 0
 		}
 	};
 
