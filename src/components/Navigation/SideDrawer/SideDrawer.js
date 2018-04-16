9cf71f58a435d2c7db5c399caed7676b95f169e7
Mark Chouinard <mark@chouinard.me>
2018-04-15 19:14:16 -0700

    Dismiss sidebar

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,29 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
 import classes from './SideDrawer.css';
+import Aux from '../../../hoc/Aux';
+import BackDrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = props => {
 
+	let drawerClasses = [ classes.SideDrawer, classes.Close ];
+
+	if ( props.open ) {
+		drawerClasses = [ classes.SideDrawer, classes.Open ];
+	}
+
 	return (
-		<div className={ classes.SideDrawer }>
-			<div className={classes.Logo}>
-				<Logo />
+		<Aux>
+			<BackDrop show={ props.open } clicked={ props.closed } />
+			<div className={ drawerClasses.join( ' ' ) }>
+				<div className={classes.Logo}>
+					<Logo />
+				</div>
+				<nav>
+					<NavItems/>
+				</nav>
 			</div>
-			<nav>
-				<NavItems/>
-			</nav>
-		</div>
+		</Aux>
 	);
 
 };
