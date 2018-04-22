fd973828777ec39fa1a1e83537209b7c0fbe6d29
Mark Chouinard <mark@chouinard.me>
2018-04-22 10:55:30 -0700

    Navigation

--- src/components/Navigation/NavItems/NavItem/NavItem.js
+++ src/components/Navigation/NavItems/NavItem/NavItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './NavItem.css';
+import { NavLink } from 'react-router-dom';
 
 const navItem = props => (
 
 	<li className={ classes.NavItem }>
-		<a href={ props.link } className={ props.active ? classes.active : '' }>
+		<NavLink to={ props.link } activeClassName={ classes.active } exact={ props.exact } >
 			{ props.children }
-		</a>
+		</NavLink>
 	</li>
 
 );
