f5c01cf4c75896fdab654c07779b40942045d67e
Mark Chouinard <mark@chouinard.me>
2018-05-12 15:17:22 -0700

    Add missing validation.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -153,6 +153,22 @@ class ContactData extends Component {
 
 		}
 
+		if ( rules.isEmail ) {
+
+			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+			isValid       = pattern.test( value ) && isValid;
+
+		}
+
+		if ( rules.isNumeric ) {
+
+			const pattern = /^\d+$/;
+
+			isValid       = pattern.test( value ) && isValid;
+
+		}
+
 		return isValid;
 
 	}
