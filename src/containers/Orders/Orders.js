31ad81a9adc5cc47e0f6e436487d084596531667
Mark Chouinard <mark@chouinard.me>
2018-05-12 18:31:09 -0700

    Commit before troubleshooting.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -10,24 +10,24 @@ class Orders extends Component {
 
 	componentDidMount() {
 
-		this.props.onFetchOrders();
+		this.props.onFetchOrders( this.props.token );
 
 	}
 
-	render () {
-		let orders = <Spinner />;
+	render() {
+		let orders = <Spinner/>;
 
-		if ( !this.props.loading ) {
+		if ( ! this.props.loading ) {
 			orders = this.props.orders.map( order => (
 				<Order
-					key={order.id}
-					ingredients={order.ingredients}
-					price={order.price} />
+					key={ order.id }
+					ingredients={ order.ingredients }
+					price={ order.price }/>
 			) )
 		}
 		return (
 			<div>
-				{orders}
+				{ orders }
 			</div>
 		);
 	}
@@ -37,13 +37,14 @@ class Orders extends Component {
 const mapStateToProps = state => {
 	return {
 		orders:  state.order.orders,
-		loading: state.order.loading
+		loading: state.order.loading,
+		token:   state.auth.token
 	};
 };
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onFetchOrders: () => dispatch( actions.fetchOrders() )
+		onFetchOrders: ( token ) => dispatch( actions.fetchOrders( token ) )
 	};
 };
 
