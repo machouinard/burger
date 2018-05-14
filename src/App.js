c82c9e9e2282fcf450a2a017515fca646c03e649
Mark Chouinard <mark@chouinard.me>
2018-05-13 17:55:55 -0700

    Protect routes from non-authenticated users.

--- src/App.js
+++ src/App.js
@@ -19,16 +19,32 @@ class App extends Component {
 
 	render() {
 
+		let routes = (
+
+			<Switch>
+				<Route path="/auth" component={ Auth }/>
+				<Route path="/" exact component={ BurgerBuilder }/>
+				<Redirect to="/"/>
+			</Switch>
+		);
+
+		if ( this.props.isAuthenticated ) {
+
+			routes = (
+				<Switch>
+					<Route path="/checkout" component={ Checkout }/>
+					<Route path="/orders" component={ Orders }/>
+					<Route path="/logout" component={ Logout }/>
+					<Route path="/" exact component={ BurgerBuilder }/>
+				</Switch>
+			)
+
+		}
+
 		return (
 			<div>
 				<Layout>
-					<Switch>
-						<Route path="/checkout" component={ Checkout }/>
-						<Route path="/orders" component={ Orders }/>
-						<Route path="/auth" component={ Auth }/>
-						<Route path="/logout" component={ Logout }/>
-						<Route path="/" exact component={ BurgerBuilder }/>
-					</Switch>
+					{ routes }
 				</Layout>
 			</div>
 		);
@@ -37,6 +53,14 @@ class App extends Component {
 
 }
 
+const mapStateToProps = state => {
+
+	return {
+		isAuthenticated: state.auth.token !== null
+	}
+
+};
+
 const mapDispatchToProps = dispatch => {
 
 	return {
@@ -44,4 +68,4 @@ const mapDispatchToProps = dispatch => {
 	}
 }
 
-export default withRouter( connect( null, mapDispatchToProps )( App ) );
+export default withRouter( connect( mapStateToProps, mapDispatchToProps )( App ) );
