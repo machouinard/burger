71d041a2c7d53c23db4a1faacaa93c6bb863629f
Mark Chouinard <mark@chouinard.me>
2018-05-14 14:01:18 -0700

    Modify for firebase deployment

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const store = createStore( rootReducer, composeEnhancers( applyMiddleware( thunk
 
 const app = (
 	<Provider store={ store }>
-		<BrowserRouter basename={ 'burger' }>
+		<BrowserRouter>
 			<App/>
 		</BrowserRouter>
 	</Provider>
