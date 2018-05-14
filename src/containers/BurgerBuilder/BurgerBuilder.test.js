fd9205448893306d038213c0dd1c5639ac32b214
Mark Chouinard <mark@chouinard.me>
2018-05-13 21:10:26 -0700

    Add container test.

---
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BurgerBuilder } from "./BurgerBuilder";
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+configure( { adapter: new Adapter() } );
+
+describe( '<BurgerBuilder />', () => {
+
+	let wrapper;
+
+	beforeEach( () => {
+		wrapper = shallow( <BurgerBuilder onInitIngredients={ () => { console.log( 'asdfasdf' ) } }/> );
+	} );
+
+	it( 'should render <BuildControls /> when recieving ingredients', () => {
+
+		wrapper.setProps( { ings: { lettuce: 0 } } );
+		expect( wrapper.find( BuildControls ) ).toHaveLength( 1 );
+
+	} );
+
+} );
